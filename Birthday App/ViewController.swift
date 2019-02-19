251fc05aa05931bdccb20ec5959aa5a48b43a81d
Kiyoto Ryuman <kiyokiyoabc@mac-the-end.local>
2019-02-19 14:32:35 +0900

    first-commit

--- Birthday App/ViewController.swift
+++ Birthday App/ViewController.swift
@@ -10,66 +10,65 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController {
     @IBOutlet var sceneView: ARSCNView!
+    
+    
+    
+    let defaultConfiguration: ARWorldTrackingConfiguration = {
+        let configuration = ARWorldTrackingConfiguration()
+        
+        let images = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+        configuration.detectionImages = images
+        
+        configuration.planeDetection = .horizontal
+        configuration.environmentTexturing = .automatic
+        return configuration
+    }()
+    
+    let imageConfiguration: ARImageTrackingConfiguration = {
+        let configuration = ARImageTrackingConfiguration()
+        
+        let images = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+        configuration.trackingImages = images!
+        return configuration
+    }()
+    
+    private var buttonNode: SCNNode!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        buttonNode = SCNScene(named: "art.scnassets/birthdaycake.scn")!.rootNode
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        sceneView.session.run(imageConfiguration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
+    
+}
 
-    // MARK: - ARSCNViewDelegate
+// MARK: - <#ARSCNViewDelegate#>
+extension ViewController: ARSCNViewDelegate {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARImageAnchor else {
+            return
+        }
+        //名刺の上にボタンを表示
+        node.addChildNode(buttonNode)
+        print("imageAnchor!")
     }
 }
+
